35b060dee6ee7fb5e34743a2bf781cc17a55084f
洛夜 <120157911+zy20221209@users.noreply.github.com>
2024-10-29 11:39:35 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+// 项目的入口，从这里开始运行
+
+// react必要的两个核心包
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+
+// 导入项目的根组件
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 把App根组件渲染到id为root的dom节点上（处于public/index.html）
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App></App>);
